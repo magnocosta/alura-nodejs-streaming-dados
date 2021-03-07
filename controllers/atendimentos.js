e96db5d830549dea367916fd7a403aff5dc9d797
Magno Costa <magno@route.com>
2021-03-07 17:52:28 -0300

    Chapter 5

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,29 @@
 const Treatment = require('../models/treatment');
 
 module.exports = app => {
-  app.get('/treatments', (req, res) => res.send('Parabens, vc esta realizando um GET'));
+  app.get('/treatments', (req, res) => {
+    Treatment.list(res);
+  });
+
+  app.get('/treatments/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    Treatment.findById(id, res);
+  });
+
   app.post('/treatments', (req, res) => {
     const treatment = req.body;
     Treatment.add(treatment, res);
-  })
+  });
+
+  app.patch('/treatments/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const values = req.body;
+    Treatment.update(id, values, res);
+  });
+
+  app.delete('/treatments/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    Treatment.delete(id, res);
+  });
+
 }
--- models/treatment.js
+++ models/treatment.js
@@ -29,15 +29,73 @@ class Treatment {
     } else {
       const newTreatment = {...treatment, created_at, scheduled_at};
       const sql = `INSERT INTO treatments SET ?`;
-      connection.query(sql, newTreatment, (error, results) => {
+      connection.query(sql, newTreatment, (error, result) => {
         if (error) {
           res.status(400).json(error);
         } else {
-          res.status(201).json(results);
+          res.status(201).json({...treatment});
         }
       });
-      }
     }
+  }
+
+  list(res) {
+    const sql = 'SELECT * FROM treatments';
+
+    connection.query(sql, (error, result) => {
+      if(error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json(result)
+      }
+    });
+
+  }
+
+  findById(id, res) {
+    const sql = `SELECT * FROM treatments WHERE id=${id}`;
+
+    connection.query(sql, (error, result) => {
+
+      if(error) {
+        res.status(400).json(error);
+      } else {
+        const treatment = result[0];
+        res.status(200).json(treatment);
+      }
+
+    });
+  }
+
+  update(id, values, res) {
+    const sql = 'UPDATE treatments SET ? WHERE id=?';
+
+    if(values.scheduled_at) {
+      values.scheduled_at = moment(values.scheduled_at, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+    }
+
+    connection.query(sql, [values, id], (error, results) => {
+
+      if(error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json({...values, id});
+      }
+
+    });
+  }
+
+  delete(id, res) {
+    const sql = 'DELETE FROM treatments WHERE id=?';
+
+    connection.query(sql, id, (error, result) => {
+      if(error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json({id});
+      }
+    })
+  }
 
 }
 
